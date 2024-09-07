c59e9d110021d18604c2fda5776b4890670f52c6
Mohamed20008 <142727290+Mohamed20008@users.noreply.github.com>
2024-09-07 06:12:30 +0300

    Add files via upload

---
+++ stages/script2.js
@@ -0,0 +1,158 @@
+// const en = document.getElementById('en');
+
+
+// en.addEventListener('click', function() {
+   
+//     document.body.innerHTML=`<style>
+//     @import url('https://fonts.googleapis.com/css2?family=Playwrite+PL:wght@100..400&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+//     body{
+   
+//     background-image: linear-gradient(to bottom right ,rgb(118, 222, 109), transparent);
+//     height: 98vh;
+// }
+// .grid{
+//     display: grid;
+    
+//     row-gap: 20px;
+//     font-family: "Roboto","Playwrite PL", cursive;
+//     grid-template-columns:repeat(1,1fr);
+//     grid-template-rows:repeat(1, 300px);
+//     font-size: 40px;
+    
+
+// }
+// a{
+//     background-color: rgba(240, 248, 255, 0.258);
+//     border-radius: 10px;
+//     padding: 40px;
+//     color: rgb(1, 82, 1);
+//     text-decoration: none;
+    
+// }
+// a:hover{
+//     box-shadow: 10px 10px 10px 1px gray;
+//     transition: 0.3s;
+// }
+// .head{
+//     height: 100px;
+//     font-family:"Roboto", "Playwrite PL", cursive;
+//     text-align: center;
+//     font-weight: bold;
+//     font-size: 45px;
+//     color: rgb(1, 82, 1);
+// }
+// </style>
+//     <div class="head">English</div>
+//     <div class="grid">
+//         <a href="https://classroom.google.com/c/NTU0NTUwMjAyMzU1?cjc=er3dxzx" > Mr Eslam </a>
+       
+//     </div>
+    
+//     `;
+// });
+// const sc = document.getElementById('sc');
+
+
+// sc.addEventListener('click', function() {
+   
+//     document.body.innerHTML=`<style>
+//     @import url('https://fonts.googleapis.com/css2?family=Playwrite+PL:wght@100..400&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+//     body{
+   
+//     background-image: linear-gradient(to bottom right ,rgb(118, 222, 109), transparent);
+//     height: 98vh;
+// }
+// .grid{
+//     display: grid;
+    
+//     row-gap: 20px;
+//     font-family: "Roboto","Playwrite PL", cursive;
+//     grid-template-columns:repeat(1,1fr);
+//     grid-template-rows:repeat(1, 300px);
+//     font-size: 40px;
+    
+
+// }
+// a{
+//     background-color: rgba(240, 248, 255, 0.258);
+//     border-radius: 10px;
+//     padding: 40px;
+//     color: rgb(1, 82, 1);
+//     text-decoration: none;
+    
+// }
+// a:hover{
+//     box-shadow: 10px 10px 10px 1px gray;
+//     transition: 0.3s;
+// }
+// .head{
+//     height: 100px;
+//     font-family:"Roboto", "Playwrite PL", cursive;
+//     text-align: center;
+//     font-weight: bold;
+//     font-size: 45px;
+//     color: rgb(1, 82, 1);
+// }
+// </style>
+//     <div class="head">Science</div>
+//     <div class="grid">
+//         <a href="https://classroom.google.com/c/NzAyNzAzOTcxNTIy?cjc=7yqpagu" > Mr name </a>
+       
+//     </div>
+    
+//     `;
+// });
+// const math = document.getElementById('math');
+
+
+// math.addEventListener('click', function() {
+   
+//     document.body.innerHTML=`<style>
+//     @import url('https://fonts.googleapis.com/css2?family=Playwrite+PL:wght@100..400&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+//     body{
+   
+//     background-image: linear-gradient(to bottom right ,rgb(118, 222, 109), transparent);
+//     height: 98vh;
+// }
+// .grid{
+//     display: grid;
+    
+//     row-gap: 20px;
+//     font-family: "Roboto","Playwrite PL", cursive;
+//     grid-template-columns:repeat(1,1fr);
+//     grid-template-rows:repeat(1, 300px);
+//     font-size: 40px;
+    
+
+// }
+// a{
+//     background-color: rgba(240, 248, 255, 0.258);
+//     border-radius: 10px;
+//     padding: 40px;
+//     color: rgb(1, 82, 1);
+//     text-decoration: none;
+    
+// }
+// a:hover{
+//     box-shadow: 10px 10px 10px 1px gray;
+//     transition: 0.3s;
+// }
+// .head{
+//     height: 100px;
+//     font-family:"Roboto", "Playwrite PL", cursive;
+//     text-align: center;
+//     font-weight: bold;
+//     font-size: 45px;
+//     color: rgb(1, 82, 1);
+// }
+// </style>
+//     <div class="head">English</div>
+//     <div class="grid">
+//         <a href="https://classroom.google.com/c/NzAxODUzNjQ0NDQx?cjc=brgg6xg" > Mr name </a>
+       
+//     </div>
+    
+//     `;
+// });
+
+
